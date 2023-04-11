3243d9f3df91e430454e92134f6602566b3117ab
josephizatt <joe@izatt.co.uk>
2023-04-11 12:47:00 +0100

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TenancyList from './TenancyList';
 import TenancyCreate from './TenancyCreate';
 import TenancyEdit from './TenancyEdit';
+import TenancyDetail from './TenancyDetail';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<TenancyList/>} />
-        <Route exact path="/create" element={<TenancyCreate/>} />
-        <Route exact path="/edit/:id" element={<TenancyEdit/>} />
+        <Route exact path="/" element={<TenancyList />} />
+        <Route exact path="/create" element={<TenancyCreate />} />
+        <Route exact path="/edit/:id" element={<TenancyEdit />} />
+        <Route exact path="/tenancies/:id/edit" element={<TenancyEdit />} />
+        <Route exact path="/tenancies/:id" element={<TenancyDetail />} />
       </Routes>
     </Router>
+
   );
 };
 
